205441ba46cba31c611f890b8318df722ca82620
CreeperLordVader-Iclucia-Ashina <1749065611@qq.com>
2024-01-20 00:09:18 +0800

    Upload new lab-8

--- lab-10-tcp_stack/auto_run.sh
+++
@@ -1,3 +0,0 @@
-make clean
-make all
-sudo python3 tcp_topo_loss.py
--- lab-5-bufferfloat/auto_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-qlens=("60" "70" "80" "90" "100" "110" "120" "130" "140" "150")
-
-for i in "${qlens[@]}"
-do
-    echo "Running with q = $i"
-    sudo python reproduce_bufferbloat.py -q $i
-    sudo python draw.py $i
-done
